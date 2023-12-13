19c5e118b78f39a65c9102ab220b292f12aaa9c8
StivenGon <oscarstiven.2005@gmail.com>
2023-12-13 19:54:51 -0300

    Ejercicio 2

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -9,6 +9,9 @@ public class Ejercicio2 {
         int tabla, result;
         System.out.print("Que tabla desea mostrar?: ");
         tabla = scanner.nextInt();
+        System.out.println("-------------------------");
+        System.out.println("Imprimiendo la tabla del: " + tabla);
+        System.out.println("-------------------------");
         for (int i=1; i<11; i++) {
             result= tabla*i;
             System.out.println(tabla + " x "+i+" = " +result);
