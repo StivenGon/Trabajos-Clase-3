4d0e722c73b99079e8a07379afa3162ee6e7fa51
StivenGon <oscarstiven.2005@gmail.com>
2023-12-13 19:52:51 -0300

    Ejercicio 1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -2,9 +2,8 @@
 // Bucle "FOR"
 public class Ejercicio1 {
     public static void main (String[] args) {
-        for (int i = 1; i < 7; i++);{
-        System.out.print("El valor es: ");
-        System.out.println("i");
+        for (int i = 1; i<7; i++){
+        System.out.println("El valors es: "+i);
         }  
     }
 }
--- Ejercicio2.java
+++ Ejercicio2.java
@@ -2,7 +2,6 @@
 //  Bucle FOR  parte 2
 
 import java.util.Scanner;
-
 public class Ejercicio2 {
 
     public static void main(String[] args) {
