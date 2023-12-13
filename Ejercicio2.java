e2205d5bab47abc681f1c7e1c18828882e41ec1c
StivenGon <oscarstiven.2005@gmail.com>
2023-12-13 19:24:57 -0300

    Ejercicio 2

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,8 +1,20 @@
 // Ejercicio 2 Practica 3
 //  Bucle FOR  parte 2
+
+import java.util.Scanner;
+
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner (System.in);
+        int tabla, result;
+        System.out.print("Que tabla desea mostrar?: ");
+        tabla = scanner.nextInt();
+        for (int i=1; i<11; i++) {
+            result= tabla*i;
+            System.out.println(tabla + " x "+i+" = " +result);
+            scanner.close();
+        }
+
     }
 }
