3ca1ec7cb30e0500c79dade0a43ba3e511d741af
StivenGon <oscarstiven.2005@gmail.com>
2023-12-13 19:03:59 -0300

    Ejercicio 2

---
+++ Ejercicio1.java
@@ -0,0 +1,10 @@
+// Ejercicio  1 Practica 3
+// Bucle "FOR"
+public class Ejercicio1 {
+    public static void main (String[] args) {
+        for (int i = 1; i < 7; i++);{
+        System.out.print("El valor es: ");
+        System.out.println("i");
+        }  
+    }
+}
---
+++ Ejercicio2.java
@@ -0,0 +1,8 @@
+// Ejercicio 2 Practica 3
+//  Bucle FOR  parte 2
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
